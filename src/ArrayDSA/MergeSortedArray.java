a3f46e44ff37f5440b671b23935ca32cd5e0e476
Vedant Pophali <officialvedantpophali2005@gmail.com>
2025-08-07 21:26:22 +0530

    Test cases added for a MergesSortedArray

--- src/ArrayDSA/MergeSortedArray.java
+++ src/ArrayDSA/MergeSortedArray.java
@@ -2,43 +2,60 @@ package ArrayDSA;
 
 import java.util.Arrays;
 
-public class MergeSortedArray {public static void merge(int[] nums1, int m, int[] nums2, int n) {
-    int i = m - 1;       // Last valid element of nums1
-    int j = n - 1;       // Last element of nums2
-    int k = m + n - 1;   // Last index of nums1 after full merge
+public class MergeSortedArray {
 
-    // Merge in reverse order
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k--] = nums1[i--];
-        } else {
-            nums1[k--] = nums2[j--];
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums2[j] > nums1[i]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 
-    // Copy remaining elements from nums2
-    while (j >= 0) {
-        nums1[k--] = nums2[j--];
-    }
-}
-
     public static void main(String[] args) {
         // Test Case 1
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
-        merge(nums1, 3, nums2, 3);
-        System.out.println("Test 1: " + Arrays.toString(nums1)); // [1, 2, 2, 3, 5, 6]
+        int[] nums1a = {1, 2, 3, 0, 0, 0};
+        int[] nums2a = {2, 5, 6};
+        merge(nums1a, 3, nums2a, 3);
+        System.out.println("Merged array 1: " + Arrays.toString(nums1a));
 
         // Test Case 2
-        int[] nums1b = {1};
-        int[] nums2b = {};
-        merge(nums1b, 1, nums2b, 0);
-        System.out.println("Test 2: " + Arrays.toString(nums1b)); // [1]
+        int[] nums1b = {4, 5, 6, 0, 0, 0};
+        int[] nums2b = {1, 2, 3};
+        merge(nums1b, 3, nums2b, 3);
+        System.out.println("Merged array 2: " + Arrays.toString(nums1b));
 
         // Test Case 3
         int[] nums1c = {0};
         int[] nums2c = {1};
         merge(nums1c, 0, nums2c, 1);
-        System.out.println("Test 3: " + Arrays.toString(nums1c)); // [1]
+        System.out.println("Merged array 3: " + Arrays.toString(nums1c));
+
+        // Test Case 4
+        int[] nums1d = {1};
+        int[] nums2d = {};
+        merge(nums1d, 1, nums2d, 0);
+        System.out.println("Merged array 4: " + Arrays.toString(nums1d));
+
+        // Test Case 5
+        int[] nums1e = {2, 0};
+        int[] nums2e = {1};
+        merge(nums1e, 1, nums2e, 1);
+        System.out.println("Merged array 5: " + Arrays.toString(nums1e));
     }
 }
